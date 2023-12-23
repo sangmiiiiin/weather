176d7e34c4568d6807dfc9db70210664126805e2
SANGMIN <kwk680037@gmail.com>
2023-12-24 01:18:27 +0900

    🔇 불필요한 콘솔 로그 제거

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,17 @@
 const API_KEY = 'h%2BP%2FXV%2FH65yFRs1Bb%2BWkv1sZw1%2BwnZWTB9k7NG1PzqnBPV%2B9OZonjBd8DhA7OJGXCFJ%2BdArTI1tgf8GBAUp8UA%3D%3D'
 const date = new Date();
 const TODAY_KEY = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate() - 1}`;
-console.log(TODAY_KEY);
 
 function weather(position) {
     const lat = Math.floor(position.coords.latitude);
     const lng = Math.floor(position.coords.longitude);
-    console.log(lat, lng);
     const url = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${API_KEY}&numOfRows=10&pageNo=1&dataType=JSON&base_date=${TODAY_KEY}&base_time=1700&nx=${lat}&ny=${lng}`
 
-    console.log(url);
     fetch(url)
         .then(response => response.json()
             .then(data => {
                 // items가 없거나 비어있으면 에러 방지를 위해 처리
                 const items = data && data.response && data.response.body && data.response.body.items && data.response.body.items.item;
-                console.log(data);
                 if (items) {
                     const temperature = items.find(item => item.category === "TMP").fcstValue;
                     console.log("Temperature:", temperature);
