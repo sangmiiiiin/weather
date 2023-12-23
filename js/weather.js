612da49058f0eb113d50950fc2d4d378de93124f
SANGMIN <kwk680037@gmail.com>
2023-12-23 23:43:27 +0900

    👽️ JSON형식으로 외부API데이터 가져오기?

--- js/weather.js
+++ js/weather.js
@@ -1,28 +1,23 @@
-var xhr = new XMLHttpRequest();
-var url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'; /*URL*/
-var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'h%2BP%2FXV%2FH65yFRs1Bb%2BWkv1sZw1%2BwnZWTB9k7NG1PzqnBPV%2B9OZonjBd8DhA7OJGXCFJ%2BdArTI1tgf8GBAUp8UA%3D%3D'; /*Service Key*/
-queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
-queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('1000'); /**/
-queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('XML'); /**/
-queryParams += '&' + encodeURIComponent('base_date') + '=' + encodeURIComponent('20231221'); /**/
-queryParams += '&' + encodeURIComponent('base_time') + '=' + encodeURIComponent('1700'); /**/
-queryParams += '&' + encodeURIComponent('nx') + '=' + encodeURIComponent('62'); /**/
-queryParams += '&' + encodeURIComponent('ny') + '=' + encodeURIComponent('127'); /**/
-xhr.open('GET', url + queryParams);
-xhr.onreadystatechange = function () {
-    if (this.readyState == 4) {
-        // console.log('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(this.responseText, 'text/xml');
-        console.log(xmlDoc);
-        const skyItem = xmlDoc.querySelector('item category:contains("SKY")').closest('item');
-        const fcstValue = skyItem.querySelector('fcstValue').textContent;
-        console.log(fcstValue);
-    }
-};
+const API_KEY = 'h%2BP%2FXV%2FH65yFRs1Bb%2BWkv1sZw1%2BwnZWTB9k7NG1PzqnBPV%2B9OZonjBd8DhA7OJGXCFJ%2BdArTI1tgf8GBAUp8UA%3D%3D'
+const date = new Date();
+const TODAY_KEY = `${date.getFullYear()}${date.getMonth()+1}${date.getDate()}`;
+console.log(TODAY_KEY);
 
-xhr.send('');
+function weather(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log(lat, lng);
+    const url = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${API_KEY}&numOfRows=10&pageNo=1&dataType=JSON&base_date=${TODAY_KEY}&base_time=1700&nx=37&ny=127`
 
+    console.log(url);
+    fetch(url)
+        .then(response => response.json()
+        .then(data => {
+            console.log(data);
+        }));
+}
+function weatherError () {
+    alert("현재 사용자의 위치 정보를 가져오지 못했습니다.");
+}
 
-
-
+navigator.geolocation.getCurrentPosition(weather, weatherError);
