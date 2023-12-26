ff5e06f9580d51274452b644fed7897b7fb7fb4b
SANGMIN <kwk680037@gmail.com>
2023-12-26 23:52:45 +0900

    ✨ 강수형태 기능 추가 및 코드 해석 로직 추가

--- js/weather.js
+++ js/weather.js
@@ -1,22 +1,22 @@
-const API_KEY = 'h%2BP%2FXV%2FH65yFRs1Bb%2BWkv1sZw1%2BwnZWTB9k7NG1PzqnBPV%2B9OZonjBd8DhA7OJGXCFJ%2BdArTI1tgf8GBAUp8UA%3D%3D'
+const API_KEY = 'h%2BP%2FXV%2FH65yFRs1Bb%2BWkv1sZw1%2BwnZWTB9k7NG1PzqnBPV%2B9OZonjBd8DhA7OJGXCFJ%2BdArTI1tgf8GBAUp8UA%3D%3D';
 const date = new Date();
 const TODAY_KEY = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`;
 console.log(date.getHours());
 let BASE_TIME_KEY;
 
-if (date.getHours() > 2 && date.getHours() < 5) {
+if (date.getHours() >= 2 && date.getHours() < 5) {
     BASE_TIME_KEY = "0200"
-} else if (date.getHours() > 5 && date.getHours() < 8) {
+} else if (date.getHours() >= 5 && date.getHours() < 8) {
     BASE_TIME_KEY = "0500"
-} else if (date.getHours() > 8 && date.getHours() < 11) {
+} else if (date.getHours() >= 8 && date.getHours() < 11) {
     BASE_TIME_KEY = "0800"
-} else if (date.getHours() > 11 && date.getHours() < 14) {
+} else if (date.getHours() >= 11 && date.getHours() < 14) {
     BASE_TIME_KEY = "1100"
-} else if (date.getHours() > 14 && date.getHours() < 17) {
+} else if (date.getHours() >= 14 && date.getHours() < 17) {
     BASE_TIME_KEY = "1400"
-} else if (date.getHours() > 17 && date.getHours() < 20) {
+} else if (date.getHours() >= 17 && date.getHours() < 20) {
     BASE_TIME_KEY = "1700"
-} else if (date.getHours() > 20 && date.getHours() < 23) {
+} else if (date.getHours() >= 20 && date.getHours() < 23) {
     BASE_TIME_KEY = "2000"
 } else if (date.getHours() >= 23) {
     BASE_TIME_KEY = "2300"
@@ -41,6 +41,7 @@ function weather(position) {
         const skyStatus = data.response.body.items.item.find(item => item.category === "SKY").fcstValue;
         const rainDrop = data.response.body.items.item.find(item => item.category === "POP").fcstValue;
         const REH = data.response.body.items.item.find(item => item.category === "REH").fcstValue;
+        const PTY = data.response.body.items.item.find(item => item.category === "PTY").fcstValue;
 
 
         weatherInfo.innerHTML = `
@@ -48,6 +49,7 @@ function weather(position) {
             <p>하늘상태: ${getSkyStatus(skyStatus)}</p>
             <p>강수확률: ${rainDrop}%</p>
             <p>습도: ${REH}%</p>
+            <p>강수형태: ${getPTY(PTY)}</p>
         `
     }
 
@@ -57,6 +59,16 @@ function weather(position) {
             case "2": return "구름조금";
             case "3": return "구름많음";
             case "4": return "흐림";
+            default: return "알 수 없음";
+        }
+    }
+    const getPTY = (code) => {
+        switch(code) {
+            case "0": return "없음";
+            case "1": return "비";
+            case "2": return "비/눈";
+            case "3": return "눈";
+            case "4": return "소나기";
             default: return "알 수 없음";
         }
     }
